a7a9da261c862cc6a4e6483b2043a1ad5c5ed20e
Kelly Limo <130097627+Lymore01@users.noreply.github.com>
2024-07-16 14:45:24 +0300

    removed proxy

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
+  /* server:{
     proxy:{
       '/api':"https://cryptic-backend.onrender.com"
     }
-  },
+  }, */
   plugins: [react()],
   define: {
     'process.env': process.env
